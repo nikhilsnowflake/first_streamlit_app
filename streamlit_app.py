5b3fac62bca2077ae100dc754e487fc17a3c995f
nikhilsnowflake <156074863+nikhilsnowflake@users.noreply.github.com>
2024-01-09 18:33:42 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale,Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' 🥣 Breakfast menu')
+streamlit.text('🥗 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🐔 Kale,Spinach and Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
